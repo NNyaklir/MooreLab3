c952d05b164881a9b48997fb988001d435269b6c
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-04-07 22:02:46 -0600

    updated testers

--- powTwoTest.c
+++ powTwoTest.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "powTwo.h"
+#include "powTwoTest.h"
 
 int runPowTest() {
     srand(time(NULL)); //rand nums
@@ -9,9 +11,28 @@ int runPowTest() {
     unsigned int num = rand();
     printf("number : ", num);
 
-    int result = powerOfTwo(num);
+    
+    //nvm just gonna hard code it
+   showTestResults(2,1);
+   showTestResults(4,2);
+   showTestResults(8,3);
+   showTestResults(16,4);
+   showTestResults(256,5);
 
+   showTestResults(38,6);
+   showTestResults(939,7);
+   showTestResults(0,8);
+   showTestResults(65,9);
+   showTestResults(1,10);
+    
+
+}
+
+int showTestResults( int num, int testNum){
+    int result = powerOfTwo(num);
+    printf ("Test %d:\n",testNum);
     if (result!=0){
+    
         printf("%u is a power of two , the log is %d. \n",num,result);
         }
     else {
--- powTwoTest.h
+++ powTwoTest.h
@@ -2,5 +2,7 @@
 #define __POWTWOTEST_H
 
 int runPowTest();
+int showTestResults( int num, int testNum);
+
 
 #endif
