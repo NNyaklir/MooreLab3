c97e9b1b5550959ebb6805b9fa22066c69432218
Devin <60894239+NNyaklir@users.noreply.github.com>
2024-04-07 21:13:50 -0600

    added powTwo

---
+++ powTwo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPowerOfTwo(unsigned int n) {
+    if (n == 0)
+        return 0;
+    
+    // Check if there's only one bit set in the binary representation
+    // A power of two will have only one bit set
+    if ((n & (n - 1)) == 0)
+        return (int)log2(n); // Return the logarithm to base 2
+    else
+        return 0; // Not a power of 2
+}
---
+++ powTwo.h
@@ -0,0 +1,6 @@
+#ifndef __POWTWO_H
+#define __POWTWO_H
+
+int isPowerOfTwo(unsigned int n);
+
+#endif
